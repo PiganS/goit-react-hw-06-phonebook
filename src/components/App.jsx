80b2c10042696a2d316750b80230bd240f62caea
PiganS <pigggggan@gmail.com>
2023-11-27 18:02:37 +0100

    1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Container, Subtitle, Title } from './App.styled';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const contactsLocalStorage = localStorage.getItem('contactList');
-    return contactsLocalStorage
-      ? JSON.parse(contactsLocalStorage)
-      : [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459 12 56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443 89 12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645 17 79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227 91 26' },
-        ];
-  });
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contactsStore.contacts);
 
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contactList', JSON.stringify(contacts));
-  }, [contacts]);
 
   const handleChange = e => {
     const { value } = e.currentTarget;
@@ -37,7 +25,12 @@ export const App = () => {
       alert(`${newContact.name} is already in contacts!`);
       return;
     }
-    setContacts(prevState => [...prevState, newContact]);
+
+    const addContactsAction = {
+      type: 'contacts/addContact',
+      payload: newContact,
+    };
+    dispatch(addContactsAction);
   };
 
   const filterElements = contacts => {
@@ -47,7 +40,11 @@ export const App = () => {
   };
 
   const handleDeleteProduct = id => {
-    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+    const deleteContactsAction = {
+      type: 'contacts/deleteContact',
+      payload: id,
+    };
+    dispatch(deleteContactsAction);
   };
 
   const filteredContacts = filterElements(contacts);
